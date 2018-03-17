7b1bd1ef29eee4f621a5351731454f4731220445
Dorian Amouroux <dor.amouroux@gmail.com>
2018-03-17 12:45:31 +0100

    fix back button in the level detail from user profile

--- levelup/Controller/UserTab/UserViewController.swift
+++ levelup/Controller/UserTab/UserViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 
-class UserViewController: UINavigationController {
+class UserViewController: UIViewController {
     
     @objc func userStatusLoaded() {
         let user = CustomUser.instance
         let identifier = user.status == .user ? "profileView" : "loginView" // choose the right VC
-        let vc:UIViewController = storyboard!.instantiateViewController(withIdentifier: identifier)
-        setViewControllers([vc], animated: false)
+        let controller:UIViewController = storyboard!.instantiateViewController(withIdentifier: identifier)
+        
+        // add the view to the page
+        addChildViewController(controller)
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(controller.view)
+        
+        // make the subview (profile or login) fill the space
+        NSLayoutConstraint.activate([
+            controller.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            controller.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            controller.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            controller.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        
+        controller.didMove(toParentViewController: self)
     }
     
     override func viewWillAppear(_ animated:Bool) {
